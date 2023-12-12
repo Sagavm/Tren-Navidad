f86c3ce17138b2f9802eaf80a2253e220a733b6b
Saúl Abello G <118303298+Sagavm@users.noreply.github.com>
2023-12-12 18:36:05 -0500

    Agregada la secuencia de lectura del sensor

--- Tren_Navideno-Aduino_nano/Tren_Navideno-Aduino_nano.ino
+++ Tren_Navideno-Aduino_nano/Tren_Navideno-Aduino_nano.ino
@@ -1,13 +1,14 @@
   #include <SoftwareSerial.h>
   #include <DFRobotDFPlayerMini.h>
-  const byte chimney = 3; //To applty PWM signal to control the fumes.
+  const byte chimney = 3; //To control the fumes.
   const byte engine = 2;
-  const byte led1 = 6, led2 = 9, led3 = 10, led4 = 11;
+  const byte led1 = 9, led2 = 10, led3 = 11, led4 = 12;
   const byte sensor = A0;
+  const byte onSensor = 4;
   byte songsQ, fumeCicle;
   int sensorVal;
   int ledStepTime = 250, fumeTime = 700;
-  unsigned long fewerFumeTime, fewerLedTime, actualTime;
+  unsigned long fewerFumeTime, fewerLedTime, fewerWaitPlayer, fewerSensorTime, actualTime;
   byte selectedSong;
   SoftwareSerial serialConnection(1, 0); // RX, TX
   DFRobotDFPlayerMini player;
@@ -20,8 +21,9 @@ void setup() {
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+  pinMode(onSensor, OUTPUT);
   pinMode(sensor, INPUT);
-  selectedSong = 1;
+  selectedSong = 3;
   fumeCicle = 1;
   
   serialConnection.begin(9600);
@@ -32,36 +34,68 @@ void setup() {
     while (true);
   }
   songsQ = player.readFileCounts();
+  digitalWrite(onSensor, HIGH);
+  fewerSensorTime = millis();
   Serial.println("Módulo DFPlayer Mini encontrado.");
   
 }
 
 void loop() {
   //Verify people presence.
+  actualTime = millis();
+  if(actualTime - fewerSensorTime >= 2000){
+    fewerSensorTime = millis();
+  }else{
+    if(actualTime - fewerSensorTime >= 1000){
+      digitalWrite(onSensor, LOW);
+    }else{
+      digitalWrite(onSensor, HIGH);
+    }
+  }
   sensorVal = analogRead(sensor);
-
   if (sensorVal >= 250){
+    digitalWrite(onSensor, LOW); //Off sensor to save energy.
     player.start();
     //Start engine.
     delay(20);//Wait till player wakes up.
-    digitalWrite(engine, HIGH);
     //Play first Song
     player.play(selectedSong);
     if (selectedSong < songsQ){
       selectedSong += 1;
     }else{
-      selectedSong = 1;
+      selectedSong = 3;
     }
     delay(20); //Wait for asking again to player.
+    //Wait for paying state, if not, keep blinking.
+    fewerWaitPlayer = millis();
+    while((player.readState() != Busy)){
+      actualTime = millis();
+      if (actualTime-fewerWaitPlayer >= 2000){
+        fewerWaitPlayer = millis();
+      }else{
+        if (actualTime-fewerWaitPlayer >= 1000){
+        digitalWrite(led1,LOW);
+        digitalWrite(led2,LOW);
+        digitalWrite(led3,LOW);
+        digitalWrite(led4,LOW);
+        }else{
+          digitalWrite(led1,HIGH);
+          digitalWrite(led2,HIGH);
+          digitalWrite(led3,HIGH);
+          digitalWrite(led4,HIGH);
+        }
+      }  
+    }
+    //Turn off leds.
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    //Start engine
+    digitalWrite(engine, HIGH);
     fewerFumeTime = millis();//Start counting time.
     fewerLedTime = fewerFumeTime;
     //While busy, show lights and fumes.
-    while((player.readState() != Busy)){
-      digitalWrite(led1,HIGH);
-      digitalWrite(led2,HIGH);
-      digitalWrite(led3,HIGH);
-      digitalWrite(led4,HIGH);
-    }
     while (player.readState() == Busy){
       //Fumes Cicle./////////////////////////////////////
       actualTime = millis();
@@ -94,7 +128,10 @@ void loop() {
               else{if (actualTime-fewerLedTime > ledStepTime*4){fewerLedTime = millis();}    
         }}}}
     }
-    //Turn Off everithing.
+    //DECIDE OF KEEPING LIGHTS AND CHIMNEY ON WHILE STOPPING
+    //Play train sound.
+    player.play(1);
+    //After finishing the song, Turn Off everithing.
     digitalWrite(led1,LOW);
     digitalWrite(led2,LOW);
     digitalWrite(led3,LOW);
@@ -105,12 +142,17 @@ void loop() {
     //Stop engine when find stop signal.
     while(1){
       if (analogRead(sensor)>600){
+        player.stop();
+        delay(20);
+        player.play(2); //Play break sound.
         delay(500);//Wait half second and stop.
         digitalWrite(engine, LOW);
         break;
       } 
     }
-    delay(30000); //Wait 30s till next reading.                                                                         
+    delay(28000); //Wait 30s till next reading.
+    player.sleep();
+    delay(2000);
+    fewerSensorTime = millis(); //Start sensor activate counter.                                                                       
   }
-  player.sleep();
 }
